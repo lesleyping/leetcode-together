fc88bc1d669f2447975e5b1b0b150225b8d20fc2
lixiping <npuxpli@mail.nwpu.edu.cn>
2019-10-21 21:26:12 +0800

    add problem10

---
+++ src/10.Regular_Expression_Matching/regular_expression_matching.cpp
@@ -0,0 +1,19 @@
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        
+    }
+};
+
+int main()
+{
+    string s = "abd";
+    string p = ".*";
+    Solution A;
+    bool res = A.isMatch(s, p);
+    cout << res << endl;
+    return 0;
+}
