947e4ec0edb54d837c789d8b63feb863cf83b342
Jian Cong <npujcong@gmail.com>
2019-11-01 11:22:12 +0800

    add problem15's solution and code

---
+++ src/15.3Sum-Array/3sum.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > threeSum(vector<int>& nums) {
+        vector<vector<int> > result;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < (nums.size()-2); ++i)
+        {
+            if(nums[i] > 0) break;
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int ll = i+1;
+            int rr = nums.size()-1;
+            while(ll < rr)
+            {
+                int sum = nums[i] + nums[ll] + nums[rr];
+                if(sum == 0)
+                {
+                    result.push_back({nums[i],nums[ll],nums[rr]});
+                    ll++;
+                    rr--;
+                }
+                else if(sum > 0)
+                {
+                    rr--;
+                }
+                else if(sum < 0)
+                {
+                    ll++;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1,2,3,0,-1};
+    Solution A;
+    vector<vector<int> > ans = A.threeSum(nums);
+    for(int i = 0; i<ans.size(); ++i)
+    {
+        for(int j = 0; j<ans[i].size(); ++j)
+        {
+            cout << ans[i][j] << endl;
+        }
+    }
+    
+    return 0;
+}
