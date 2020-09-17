8c336e4887dd039414773cd62fe9f433057d9a6e
lesleyping <741178981@qq.com>
2020-09-17 10:15:32 +0800

    finish hash

--- src/0000Explore/Hash.java
+++ src/0000Explore/Hash.java
@@ -332,3 +332,278 @@ class Solution {
     }
 }
 //11
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(int i = 0; i < strs.length; i++){
+            char[] tmp = strs[i].toCharArray();
+            Arrays.sort(tmp);
+            String root = new String(tmp);
+            if(map.containsKey(root)){
+                map.get(root).add(strs[i]);
+            }else{
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                map.put(root, list);
+            }
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
+//12
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] row = new int[9][9];
+        int[][] col = new int[9][9];
+        int[][] box = new int[9][9];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == '.'){
+                    continue;
+                }
+                int index = (i/3) * 3 + j/3;
+                int val = board[i][j] - '1';
+                if(row[i][val] == 0 && col[j][val] == 0 && box[index][val] == 0){
+                    row[i][val] = 1;
+                    col[j][val] = 1;
+                    box[index][val] = 1;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+//13
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<String,Integer> map = new HashMap<>();
+    List<TreeNode> res = new ArrayList<>();
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        
+        dfs(root);
+        return res;
+    }
+    private String dfs(TreeNode root){
+        if(root == null){
+            return "";
+        } 
+        String tmp = String.valueOf(root.val) + "," + 
+            dfs(root.left) + "," + dfs(root.right);
+        
+        if(map.containsKey(tmp)){
+            if(map.get(tmp) == 1){
+                res.add(root);
+            } 
+            map.put(tmp, map.get(tmp) + 1);
+        }else{
+            map.put(tmp, 1);
+        }
+        return tmp;
+    }
+}
+//14
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> set = new HashSet<>();
+        char[] strj = J.toCharArray();
+        char[] strs = S.toCharArray();
+        int count = 0;
+        for(int i = 0; i < strj.length; i++){
+            set.add(strj[i]);
+        }
+        for(int i = 0; i < strs.length; i++){
+            if(set.contains(strs[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+//15
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if("".equals(s)) return 0;
+        int ll = 0;
+        int maxlen = Integer.MIN_VALUE;
+        Set<Character> set = new HashSet<>();
+        char[] strs = s.toCharArray();
+        for(int i = 0; i < strs.length; i++){
+            while(set.contains(strs[i])){
+                set.remove(strs[ll]);
+                ll++;
+            }
+            maxlen = Math.max(maxlen, i - ll + 1);
+            set.add(strs[i]);
+        }
+        return maxlen;
+    }
+}
+//可以用charAt！！！！
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0) return 0;
+        int ll = 0;
+        int maxlen = Integer.MIN_VALUE;
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++){
+            while(set.contains(s.charAt(i))){
+                set.remove(s.charAt(ll));
+                ll++;
+            }
+            maxlen = Math.max(maxlen, i - ll + 1);
+            set.add(s.charAt(i));
+        }
+        return maxlen;
+    }
+}
+//16
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> sumAB = new HashMap<>();
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < B.length; j++){
+                int sum = A[i] + B[j];
+                if(sumAB.containsKey(sum)){
+                    sumAB.put(sum, sumAB.get(sum) + 1);
+                }else{
+                    sumAB.put(sum, 1);
+                }
+            }
+        }
+        int res = 0;
+        for(int i = 0; i < C.length; i++){
+            for(int j = 0; j < D.length; j++){
+                int tmp = -(C[i] + D[j]);
+                if(sumAB.containsKey(tmp)){
+                    res += sumAB.get(tmp);
+                }
+            }
+        }
+        return res;
+    }
+}
+//17-1
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b){
+                return map.get(a) - map.get(b);
+            }
+        });
+        for(Integer key : map.keySet()){
+            if(pq.size() < k){
+                //System.out.println("1:"+key);
+                pq.offer(key);
+            }else if(map.get(key) > map.get(pq.peek())){
+                //System.out.println("2:"+key);
+                pq.poll();
+                pq.offer(key);
+            }
+        }
+        int size = pq.size();
+        int[] res = new int[size];
+        for(int i = 0; i < size; i++){
+            res[i] = pq.poll();
+        }
+        return res;
+    }
+}
+//17-2
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        List<Integer>[] freqList = new List[nums.length+1];
+        for(int i = 0; i < nums.length+1; i++){
+            freqList[i] = new ArrayList<>();
+        }
+        for(int key : map.keySet()){
+            freqList[map.get(key)].add(key);
+        }
+        int idx = 0;
+        int[] res = new int[k];
+        for(int i = nums.length; i >= 0; i--){
+            if(freqList[i] == null){
+                continue;
+            }
+            for(int num : freqList[i]){
+                res[idx++] = num;
+                if(idx == k){
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+
+//18
+class RandomizedSet {
+    Map<Integer,Integer> map = new HashMap<>();
+    List<Integer> value = new ArrayList<>();
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(map.containsKey(val)){
+            return false;
+        }
+        map.put(val, value.size());
+        value.add(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!map.containsKey(val)){
+            return false;
+        }
+        int size = value.size();
+        int loc = map.get(val);
+        map.put(value.get(size-1),loc);
+        map.remove(val);
+        value.set(loc, value.get(size-1));
+        value.remove(size-1);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        Random rand = new Random();
+        return value.get(rand.nextInt(value.size()));
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ *izedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ */
